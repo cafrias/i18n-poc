6c77e899d398ed57d43f9dbab9da6e8946129b11
Carlos Frias <carlos.a.frias@gmail.com>
2021-09-28 14:30:59 -0300

    Add multiple region support

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,18 @@
+const { REGION } = process.env;
+
+let i18n;
+switch (REGION) {
+  case "us":
+    console.log("Using US region configuration");
+    i18n = require("./config/i18n/i18n.us");
+    break;
+  default:
+    console.log("Using default region configuration");
+    i18n = require("./config/i18n/i18n.default");
+}
+
 module.exports = {
-  i18n: {
-    locales: ["en", "es", "de"],
-    defaultLocale: "de",
-  },
+  i18n,
+  // NOTE: we leave this file here for further configuration
+  // of the plugin.
 };
